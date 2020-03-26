c67fd9710d38e206f14ccee37b6cebea6874d656
shaojuanzhang <rollingcomma@gmail.com>
2020-03-26 13:07:13 -0700

    integrating test1 passed: new user and post

--- src/db_server/app.js
+++ src/db_server/app.js
@@ -4,12 +4,12 @@ const express = require("express"),
 
 module.exports = (database) => {
     const dbQuery = require("./query")(database);
-    app.use("/", dbQuery);
-    
     app.use(express.json());
     app.use(bodyParser.urlencoded({
         extended: true
     }));
     app.use(bodyParser.json());
+    app.use("/", dbQuery);
+    
     return app;
 };
